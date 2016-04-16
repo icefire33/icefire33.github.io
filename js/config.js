6e4c8db1aafe1085c51b5daeffb9fc9ca9f12a38
xusha33 <1140943075@qq.com>
2016-04-16 20:29:24 +0800

    seajs引入

---
+++ js/config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2016/4/16.
+ */
+seajs.config({
+
+    // 别名配置
+    alias: {
+        'jquery': 'jquery-1.11.3',
+        'bootstrap': 'bootstrap.min'
+    },
+
+
+    // 文件编码
+    charset: 'utf-8'
+});
+//seajs.host="http://localhost:8081";
+seajs.host = "http://icefirevie.github.io"; //远程接口服务的地址
+seajs.pageSize = 20;
---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/4/16.
+ */
+define(function(require,exports,module){
+    var $=require("jquery");
+    var bootstrap=require("bootstrap");
+    var indexPage={};
+    indexPage.pageInit=function(){
+        $("body").load("html/login.html",function(){});
+    };
+    exports.init=indexPage;
+});
