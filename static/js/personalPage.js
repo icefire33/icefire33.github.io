a68eb6706506addf3c87a6902171389d9ebde60c
xusha33 <1140943075@qq.com>
2016-04-20 16:02:06 +0800

    左右轮播

--- static/js/personalPage.js
+++ static/js/personalPage.js
@@ -25,13 +25,23 @@ define(function (require, exports, module) {
     function bindArrowsClick() {
         $(".rightScroll").on("click", function () {
             $(".mainPictureBox li").each(function(i,n){
-                $(n).removeClass("moveScrollOne moveScrollTwo moveScrollThree moveScrollFour");
+                $(n).removeClass("moveScrollOne moveScrollTwo moveScrollThree moveScrollFour moveScrollOneR moveScrollTwoR moveScrollThreeR moveScrollFourR");
             });
-             $(".mainPictureBox li:first-child").addClass("moveScrollOne").css({left:"29%", zIndex:-2,transform: "translate3D(0, 0, -400px) rotateY(0)",right:"auto"});
+             $(".mainPictureBox li:first-child").addClass("moveScrollOne").css({left:"29%", zIndex:-10,transform: "translate3D(0, 0, -400px) rotateY(0)",right:"auto"});
              $(".mainPictureBox li:nth-child(2)").addClass("moveScrollTwo").css({left:"-12%", zIndex:-1,transform: "translate3D(0, 0, -400px) rotateY(30deg)",right:"auto"});
              $(".mainPictureBox li:nth-child(3)").addClass("moveScrollThree").css({right:"29%", zIndex:100,transform: "translate3D(0, 0, 0) rotateY(0deg)",left:"auto"});
              $(".mainPictureBox li:nth-child(4)").addClass("moveScrollFour").css({right:"-12%",zIndex:-1, transform: "translate3D(0, 0, -400px) rotateY(-30deg)",left:"auto"});
             $(".mainPictureBox").append($(".mainPictureBox li").first());
+        });
+        $(".leftScroll").on("click", function () {
+            $(".mainPictureBox li").each(function(i,n){
+                $(n).removeClass("moveScrollOneR moveScrollTwoR moveScrollThreeR moveScrollFourR moveScrollOne moveScrollTwo moveScrollThree moveScrollFour");
+            });
+            $(".mainPictureBox li:first-child").addClass("moveScrollOneR").css({left:"29%", zIndex:100,transform: "translate3D(0, 0, 0) rotateY(0)",right:"auto"});
+            $(".mainPictureBox li:nth-child(2)").addClass("moveScrollTwoR").css({right:"-12%", zIndex:-1,transform: "translate3D(0, 0, -400px) rotateY(-30deg)",left:"auto"});
+            $(".mainPictureBox li:nth-child(3)").addClass("moveScrollThreeR").css({right:"29%", zIndex:-10,transform: "translate3D(0, 0, -400px) rotateY(0deg)",left:"auto"});
+            $(".mainPictureBox li:nth-child(4)").addClass("moveScrollFourR").css({left:"-12%",zIndex:-1, transform: "translate3D(0, 0, -400px) rotateY(30deg)",right:"auto"});
+            $(".mainPictureBox li:first-child").before($(".mainPictureBox li").last());
         });
     }
 
